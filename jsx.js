e51b4827dd50f0ba5f30c6a1cbb5abb83a876a25
designerCity <97098829+designerCity@users.noreply.github.com>
2022-02-09 01:50:53 +0900

    Create jsx.js

---
+++ jsx.js
@@ -0,0 +1,8 @@
+// index.js 파일이라고 가정
+
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1 id="hello">Hi React!<h1>, document.getElementById('root'));
+
+// html 과 js 문법을 섞어쓸 수 있는 JS의 확장된 문법
+// 일반적인 태그처럼, id를 부여할 수도 
