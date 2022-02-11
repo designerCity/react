6855de023ace6b92c4e84f42740dfe168d0e8b7b
designerCity <97098829+designerCity@users.noreply.github.com>
2022-02-11 17:42:21 +0900

    Update jsx.js

--- jsx.js
+++ jsx.js
@@ -33,10 +33,8 @@ ReactDOM.render(
 // div 나 다른 태그로 묶고 싶지 않을 때
 // Fragment 사용 
 
-// 방법
+// 방법 1
 import { Fragment } from 'react';
-
-
 ReactDOM.render(
   <Fragment>
     <p className="hello">안녕 리엑트</p>
@@ -45,11 +43,23 @@ ReactDOM.render(
   </Fragment>,
   document.getElementById('root')
 );
-// 이렇게 사용하면됨 
+// 이렇게 사용하면됨 ==> 불필요한 div 태그를 줄일 수 있다. 
+// 방법 2
+ReactDOM.render(
+  <>
+    <p className="hello">안녕 리엑트</p>
+    <label htmlFor="name">이름 </label>
+    <input id="name" type="text"/>
+  </>,
+  document.getElementById('root')
+);
+// import 로 불러오지 않고, 축약형 문법으로 이름없는 태그로 사용하는 Fragment 사용 방법이 있다.
+
+
 
 // JSX 문법 정리
 //
 // html 에서 class 라는 속성을 사용하려면 className 라는 속성을 작성해야한다. 
 // JSX 문법으로 html for 라는 속성을 사용하려면, htmlFor 라고 작성해야함.
 // html 에서 소문자로 작성하였던 이벤트 핸들러들은 camelCase를 사용해야함 
-// JSX 문법으로 html 문법을 작성할 때에는 반드시 하나로 감싸진 태그로 작성을 해야함 or Fragment 
+// JSX 문법으로 html 문법을 작성할 때에는 반드시 하나로 감싸진 태그로 작성을 해야함 or Fragment 사용으로 불필요한 div 태그를 줄일 수 있다.
