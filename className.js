1195be0603ce26c56826d9a27c63a501f88098af
designerCity <97098829+designerCity@users.noreply.github.com>
2022-02-18 19:17:04 +0900

    Update className.js

--- className.js
+++ className.js
@@ -100,3 +100,60 @@ function App() {
 export default App;
 
 
+
+// 디자인을 적용하는 방법
+// 이미지 불러오기
+// 이미지 파일은 import 구문을 통해 불러오고, 불러온 이미지 주소를 src 속성으로 사용하면 됨.
+
+import diceImg from './assets/dice.png';
+
+function Dice() {
+  return <img src={diceImg} alt="주사위 이미지" />;
+}
+
+export default App;
+
+
+// 인라인 스타일
+// 리액트에서 인라인 스타일은 문자열이 아닌 객체형으로 사용합니다. 프로퍼티 이름은 CSS 속성 이름으로, 프로퍼티 값은 CSS 속성 값으로 쓰는데요, 
+// 이때 프로퍼티 이름은 아래의 boarderRadius 처럼 대시 기호 없이 카멜 케이스로
+import diceImg from './assets/dice.png';
+
+const style = {
+  borderRadius: '50%',
+  width: '120px',
+  height: '120px',
+};
+
+function Dice() {
+  return <img style={style} src={diceImg} alt="주사위 이미지" />;
+}
+
+export default App;
+
+// CSS 파일 불러오기
+// import 구문으로 파일을 불러올 수 있는데요, 이때 from 키워드 없이 쓰면 됩니다.
+import diceImg from './assets/dice.png';
+import './Dice.css';
+
+function Dice() {
+  return <img src={diceImg} alt="주사위 이미지" />;
+}
+
+export default App;
+
+// 클래스네임 사용하기
+// CSS 파일에 정의된 클래스명을 className prop에 문자열로 넣어주면 됩니다. 
+// 이때 재사용성을 위해 className prop을 부모 컴포넌트에서 받으면 더 좋습니다.
+import diceImg from './assets/dice.png';
+import './Dice.css';
+
+function Dice({ className = '' }) {
+  const classNames = `Dice ${className}`;
+  return <img className={classNames} src={diceImg} alt="주사위 이미지" />;
+}
+
+export default App;
+
+
+//classnames라는 라이브러리를 활용하면 조금 더 개발생산성을 높일 수 있다. 
