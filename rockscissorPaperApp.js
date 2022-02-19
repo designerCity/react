c5a538dd34e90408cbe55ba0ab03f168071de9c6
designerCity <97098829+designerCity@users.noreply.github.com>
2022-02-20 01:00:26 +0900

    Update rockscissorPaperApp.js
    갓-벽

--- rockscissorPaperApp.js
+++ rockscissorPaperApp.js
@@ -6,8 +6,9 @@ import { compareHand, generateRandomHand } from './utils';
 import './App.css'
 import Reset from './assets/ic-reset.svg'
 
-const INITIAL_VALUE = 'rock';
 
+const INITIAL_VALUE = 'rock';
+const INITIAL_Img = 'Hand';
 // 승패 문자로 결과 내주는 함수  
 function getResult(me, other) {
   const comparison = compareHand(me, other);
@@ -18,25 +19,33 @@ function getResult(me, other) {
 
 
 function App() {
+  // 최초 가위바위보 state
   const [hand, setHand] = useState(INITIAL_VALUE);
   const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
   const [gameHistory, setGameHistory] = useState([]);
-
+  // 최초 score state
   const [score, setScore] = useState(0);
   const [otherScore, setOtherScore] = useState(0);
   const [bet, setBet] = useState(1);
+
+  // 최초 승패 state 
+  const [meWin, setMeWin] = useState(INITIAL_Img);
+  const [otherWin, setOtherWin] = useState(INITIAL_Img);
+  // const [draw, setDraw] = useState(INITIAL_Img);
+
 
   // 가위바위보를 냈을 때 문자열 숫자 return 
   const handleButtonClick = (nextHand) => {
     const nextOtherHand = generateRandomHand(); // 랜덤하게 상대 가위바위보 내게 
     const nextHistoryItem = getResult(nextHand, nextOtherHand); // 문자열로 결과
     const comparison = compareHand(nextHand, nextOtherHand); // 숫자 1, -1, 0 (승 패 무)
-
+    
     setHand(nextHand);
     setOtherHand(nextOtherHand);
     setGameHistory([...gameHistory, nextHistoryItem]);
-    if (comparison > 0) setScore(score + bet);
-    if (comparison < 0) setOtherScore(otherScore + bet);
+    if (comparison > 0) return (setScore(score + bet), setMeWin('Hand winner'), setOtherWin('Hand'));
+    if (comparison < 0) return  (setOtherScore(otherScore + bet), setOtherWin('Hand winner'), setMeWin('Hand'));
+    return (setMeWin('Hand'), setOtherWin('Hand'))
   };
 
   // 처음부터 버튼
@@ -48,7 +57,7 @@ function App() {
     setOtherScore(0);
     setBet(1);
   };
-
+  // 배팅을 할 수 있는 숫자를 만드는 함수 handleBetChange
   const handleBetChange = (e) => {
     let num = Number(e.target.value);
     if (num > 9) num %= 10;
@@ -60,13 +69,11 @@ function App() {
     <div className='App'>
       <h1 className='App-heading'>가위바위보</h1>
       <Button className='App-reset' onClick={handleClearClick}><img className='App-reset' src={ Reset } alt='초기화' /></Button>
-
       <div className='App-scores'>
         <div className='Score'>
           <div className='Score-num'> {score}</div>
           <div className='Score-name'> 나</div>
         </div>
-
         <div className='App-versus'> : </div>
         <div className='Score'>
           <div className='Score-num'> {otherScore}</div>
@@ -77,9 +84,9 @@ function App() {
       <div className='Box App-box'>
         <div className='Box-inner'>
           <div className='App-hands'>
-            <div className='Hand'> <HandIcon className="Hand Hand-icon" value={hand} /> </div>
+            <div className={meWin}> <HandIcon className="Hand-icon" value={hand} /> </div>
             <div className='App-versus'> VS </div>
-            <div className='Hand'> <HandIcon className="Hand-icon" value={otherHand} /> </div>
+            <div className={otherWin}> <HandIcon className="Hand-icon" value={otherHand} /> </div>
           </div>
         </div>
       </div>
